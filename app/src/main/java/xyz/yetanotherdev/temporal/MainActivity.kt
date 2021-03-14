4e37588be5c802f5f5022bb790d7e27ca46e6565
biinui <yu.alvinray@gmail.com>
2021-03-14 12:18:18 +0800

    Initial commit

---
+++ app/src/main/java/xyz/yetanotherdev/temporal/MainActivity.kt
@@ -0,0 +1,18 @@
+package xyz.yetanotherdev.temporal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.yetanotherdev.temporal.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/xyz/yetanotherdev/temporal/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package xyz.yetanotherdev.temporal.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
